751f72cfbaf095a64eaf882bfcae6f9b27d06ecd
William West <wwest466@gmail.com>
2020-10-25 17:37:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,6 +83,18 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql).map do|row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
